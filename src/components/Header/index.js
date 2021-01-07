29ba8e075be8d228235f102b28c37117a5880bb4
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-06 20:23:31 -0500

    updates nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,16 +4,16 @@ import styles from './Header.module.css';
 
 const Header = (props) => {
 
-    let nav = props.user ?
-    <div>
-        <Link to='/create-guide' className={styles.NavBarLink}>Create Guide</Link>
-        <Link to='' className={styles.NavBarLink} onClick={props.handleLogout}>Log Out</Link>
-    </div>
-    :
-    <div>
-        <Link to='/login' className={styles.NavBarLink}>Log In</Link>
-        <Link to='/signup' className={styles.NavBarLink}>Sign Up</Link>
-    </div>;
+    // let nav = props.user ?
+    // <div>
+    //     <Link to='/create-guide' className={styles.NavBarLink}>Create Guide</Link>
+    //     <Link to='' className={styles.NavBarLink} onClick={props.handleLogout}>Log Out</Link>
+    // </div>
+    // :
+    // <div>
+    //     <Link to='/login' className={styles.NavBarLink}>Log In</Link>
+    //     <Link to='/signup' className={styles.NavBarLink}>Sign Up</Link>
+    // </div>;
 
     return (
         <header className={styles.Header}>
