a87f9719d3980be6ab207030bdcfb138a728f17c
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-06 19:52:37 -0500

    adds text to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,11 @@
+import styles from './Footer.module.css';
+
 const Footer = (props) => {
     return (
-        <footer>
-            Footer
+        <footer className={styles.Footer}>
+            <p>Copyright &copy; All rights reserved {new Date().getFullYear()} League Elite</p>
         </footer>
-    )
-}
+    );
+};
 
 export default Footer;
