7b9b9a5dddd823ea194f0bd60a0524e664c5325d
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-09 17:41:04 -0500

    Displays Champions name, title, and stats

--- src/pages/ChampionsPage/index.js
+++ src/pages/ChampionsPage/index.js
@@ -1,7 +1,12 @@
+import ChampionsCard from '../../components/ChampionsCard';
+
+import { Link } from 'react-router-dom';
+import { styles } from './ChampionsPage.module.css'
+
 const ChampionsPage = (props) => {
     return (
         <div className="ChampionsPage">
-
+            <ChampionsCard Champions={props.Champions}/>
         </div>
     )
 }
