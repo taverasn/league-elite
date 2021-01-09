f796870f6a4c2165119bfd001d0542feb96902c0
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-09 17:42:24 -0500

    Adds Champions Link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,17 +4,6 @@ import styles from './Header.module.css';
 
 const Header = (props) => {
 
-    // let nav = props.user ?
-    // <div>
-    //     <Link to='/create-guide' className={styles.NavBarLink}>Create Guide</Link>
-    //     <Link to='' className={styles.NavBarLink} onClick={props.handleLogout}>Log Out</Link>
-    // </div>
-    // :
-    // <div>
-    //     <Link to='/login' className={styles.NavBarLink}>Log In</Link>
-    //     <Link to='/signup' className={styles.NavBarLink}>Sign Up</Link>
-    // </div>;
-
     return (
         <header className={styles.Header}>
             <Link to="/">
@@ -22,18 +11,29 @@ const Header = (props) => {
             </Link>
             <nav>
                 <ul>
-                    <li>
-                        <Link to="/signup">Sign Up</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/logout">Log Out</Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard">Dashboard</Link>
-                    </li>
+                    {
+                        props.user ?
+                        <>
+                        <li>
+                            <Link to="/" onClick={props.handleLogout}>Log Out</Link>
+                        </li>
+                        <li>
+                            <Link to="/dashboard">Dashboard</Link>
+                        </li>
+                        <li>
+                            <Link to="/champions">Champions</Link>
+                        </li>
+                        </>
+                        :
+                        <>
+                        <li>
+                            <Link to="/signup">Sign Up</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        </>
+                    }
                 </ul>
             </nav>
         </header>
