42ff7114e008d4c1c3f39434bd1479f6f80de924
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-12 22:03:05 -0500

    Removes get single champion function

--- src/services/lol-api.js
+++ src/services/lol-api.js
@@ -1,8 +1,19 @@
-const BASE_URL = 'http://ddragon.leagueoflegends.com/cdn/11.1.1/data/en_US/champion.json';
+const BASE_URL = 'http://ddragon.leagueoflegends.com/cdn/11.1.1/data/en_US/';
 
 // functions
 
-export function getChampions() {
-    return fetch(BASE_URL)
+function getChampions() {
+    return fetch(BASE_URL + 'champion.json')
     .then(res => res.json());
+}
+
+
+function getSingleChampion(championName) {
+    return fetch(BASE_URL + 'champion/' + championName + '.json')
+    .then(res => res.json());
+}
+
+export {
+    getChampions,
+    getSingleChampion
 }
