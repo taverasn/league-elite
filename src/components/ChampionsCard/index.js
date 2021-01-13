d8a4835f65858f9dbafb826caf43e78aaf164b3b
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-12 22:09:18 -0500

    Changes all champion portraits to modals

--- src/components/ChampionsCard/index.js
+++ src/components/ChampionsCard/index.js
@@ -1,28 +1,23 @@
+import ChampionModal from '../ChampionModal';
 import styles from './ChampionsCard.module.css';
-import { Link } from 'react-router-dom';
 
-import { useState, useEffect } from 'react';
+
+
 
 const ChampionsCard = (props) => {
 
-
-
     return (
-        <div>
-            {props.Champions.map((champion, idx) =>
-            <section
-            key={idx}
-            >
-                <h1>{champion.name}</h1>
-                <h3>The {champion.title}</h3>
-                <p>Armor: {champion.stats.armor}</p>
-                <p>Attack Damage: {champion.stats.attackdamage}</p>
-                <p>Attack Range: {champion.stats.attackrange}</p>
-                <p>HP: {champion.stats.hp}</p>
-                <p>Mana: {champion.stats.mp}</p>
-            </section>
-            )}
+        <div className={"Page" + styles.ChampionCards}>
+            <div className={styles.ChampionCard}>
+                {props.Champions.map((champion, idx) =>
+                <ChampionModal
+                className={styles.Modal}
+                champion={champion}
+                key={idx}
+                />
 
+                )}
+            </div>
         </div>
     );
 };
