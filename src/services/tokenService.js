2d9defd61aa7913def07161cd0445cccd1faba16
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-12 22:04:00 -0500

    Added eslint-disable-next-line to stop meaningless error from displaying

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -21,6 +21,7 @@ function getToken() {
         const payload = JSON.parse(atob(token.split('.')[1]));
         if(payload.exp < Date.now() / 1000) {
             removeToken();
+            // eslint-disable-next-line
             token = null;
         }
     }
