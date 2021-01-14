35fd6f53ee1de36e9dedd4b78cac7b403e2db4e2
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-14 06:27:48 -0500

    Imports Guides, CreateGuide, Edit Guide Component and adds functions for Full CRUD

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-// Component Imports
+// Page Imports
 import HomePage from './pages/HomePage';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashBoardPage';
 import ChampionsPage from './pages/ChampionsPage';
+import GuidesPage from './pages/GuidesPage';
+import CreateGuidePage from './pages/CreateGuidePage';
+import EditGuidePage from './pages/CreateGuidePage';
+
+// Component Imports
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -17,6 +22,7 @@ import { useState, useEffect } from 'react';
 // Service Imports
 import { getUser, logout } from './services/userService';
 import { getChampions} from './services/lol-api';
+import { fetchGuideData, addGuideData } from './services/guideService';
 
 
 function App(props) {
@@ -40,20 +46,7 @@ function App(props) {
   }
 
 
-  const [championsData, setChampionsData] = useState({
-    blurb: null,
-    id: null,
-    image: null,
-    info: null,
-    key: null,
-    name: null,
-    partype: null,
-    stats: null,
-    tags: null,
-    title: null,
-    version: null,
-    searchTerm:'',
-  })
+  const [championsData, setChampionsData] = useState([])
 
   async function getChampionsData() {
     const data = await getChampions();
@@ -66,6 +59,47 @@ function App(props) {
     getChampionsData();
     console.log('effect');
   }, []);
+
+  const [ guideData, setGuideData ] = useState([]);
+
+  useEffect(() => {
+      getGuides();
+  }, []);
+
+  async function getGuides() {
+    const data = await fetchGuideData();
+    setGuideData(data);
+    console.log(data);
+  }
+
+  async function createGuide(guide) {
+    const data = await addGuideData(guide);
+    setGuideData(data);
+    console.log(data);
+  }
+
+  const deleteGuide = (id) => {
+    setEditing(false)
+
+    setGuideData(guideData.filter((guide) => guide.id !== id))
+  }
+
+  const [ editing, setEditing ] = useState(false)
+  const initialFormState = { id: null, name: "", type: "" }
+
+  const [ currentGuide, setCurrentGuide ] = useState(initialFormState)
+
+  const editRow = (guide) => {
+    setEditing(true)
+
+    setCurrentGuide({ id: guide.id, name: guide.name, type: guide.type })
+  }
+
+  const updateGuide = (id, updatedGuide) => {
+    setEditing(false)
+
+    setGuideData(guideData.map((guide) => (guide.id === id ? updatedGuide : guide)))
+  }
 
   return (
     <div className="App">
@@ -100,6 +134,41 @@ function App(props) {
               />
               :
               <Redirect to="/login"/>
+            } />             
+            <Route exact path='/guides' render={props =>
+              userState.user ?
+              <GuidesPage 
+              {...props}
+              guides={guideData}
+              editRow={editRow}
+              deleteGuide={deleteGuide}
+              />
+              :
+              <Redirect to="/login"/>
+            } />             
+            <Route exact path='/createguide' render={props =>
+              userState.user ?
+              <CreateGuidePage 
+              {...props}
+              createGuide={createGuide}
+              guides={guideData}
+              />
+              :
+              <Redirect to="/login"/>
+            } />             
+            <Route exact path='/editguide' render={props =>
+              editing ? 
+              <EditGuidePage 
+              {...props}
+              setEditing={setEditing}
+              currentGuide={currentGuide}
+              updateGuide={updateGuide}
+              createGuide={createGuide}
+              guides={guideData}
+              />
+              :
+              <Redirect 
+              to="/createguide"/>
             } />             
           </Switch>
         </main>
