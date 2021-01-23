9d37fc868364fde3ef739442fa76ae3f0829ba72
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-23 17:10:34 -0500

    Change to base url

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,7 +3,7 @@ import { setToken, getUserFromToken, removeToken } from './tokenService';
 
 // Variables
 // const BASE_URL = 'http://localhost:3001/api/users';
-const BASE_URL = 'https://league-elite-backend.herokuapp.com/api/users';
+const DATABASE_BASE_URL = 'https://league-elite-backend.herokuapp.com/api/users';
 
 
 // Functions
