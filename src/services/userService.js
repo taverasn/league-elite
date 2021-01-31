34b8356f7d7a59742d9fc3c4c5b1ebb542b90d89
Nicholas Taveras III <taveras123n@gmail.com>
2021-01-31 10:15:26 -0500

    change to database url

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,8 +2,8 @@
 import { setToken, getUserFromToken, removeToken } from './tokenService';
 
 // Variables
-const DATABASE_BASE_URL = 'http://localhost:3001/api/users/';
-// const DATABASE_BASE_URL = 'https://league-elite-backend.herokuapp.com/api/users';
+// const DATABASE_BASE_URL = 'http://localhost:3001/api/users/';
+const DATABASE_BASE_URL = 'https://league-elite-backend.herokuapp.com/api/users';
 
 
 // Functions
