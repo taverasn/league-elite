7de12b33c2bd2be339b89a680743f4f602d768cc
Nicholas Taveras III <taveras123n@gmail.com>
2021-03-28 19:36:53 -0400

    temporary removal of modal and replaced by link that opens the champion page until another use for the modal is found

--- src/components/ChampionsCard/index.js
+++ src/components/ChampionsCard/index.js
@@ -1,8 +1,8 @@
 // Styling Imports
 import styled from 'styled-components';
 
-// Component Imports
-import ChampionModal from '../ChampionModal';
+// React Components
+import { Link } from 'react-router-dom';
 
 // Styled Component
 const StyledChampionCard = styled.div`
@@ -13,6 +13,16 @@ const StyledChampionCard = styled.div`
         padding: 10px;
     }
 `;
+const StyledLinkDiv = styled.div`
+    flex-direction: column;
+    text-align: center;
+    border-color: red;
+    margin: 10px;
+    img {
+        border-radius: 50%;
+    }
+`;
+
 
 
 const ChampionsCard = (props) => {
@@ -20,10 +30,18 @@ const ChampionsCard = (props) => {
         <StyledChampionCard>
             <div>
                 {props.champions.map((champion, idx) =>
-                <ChampionModal
-                champion={champion}
-                key={champion.name}
-                />
+                
+                    <StyledLinkDiv key={champion.id}>
+                        <Link
+                        to={`/champions/${champion.id}`}
+                        >
+                        <img 
+                            src={"https://ddragon.leagueoflegends.com/cdn/11.1.1/img/champion/" + champion.id + ".png"} 
+                            alt=""
+                        ></img>
+                            <p>{champion.name}</p>  
+                        </Link>
+                    </StyledLinkDiv>
                 )}
             </div>
         </StyledChampionCard>
