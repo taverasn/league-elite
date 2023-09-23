35e3b1657445348a9b12a99c0f96cf354c68f1ca
Nicholas Taveras <taveras123n@gmail.com>
2023-09-23 02:13:42 -0400

    testing for local backend

--- src/services/guideService.js
+++ src/services/guideService.js
@@ -1,78 +1,78 @@
 // Imports
-import { getToken } from './tokenService';
+import { getToken } from "./tokenService";
 
 // Variables
-// const GUIDES_BASE_URL = 'http://localhost:3001/api/guides/';
-const GUIDES_BASE_URL = 'https://league-elite-backend.herokuapp.com/api/guides/';
+const GUIDES_BASE_URL = "http://localhost:3001/api/guides/";
+//const GUIDES_BASE_URL = 'https://league-elite-backend.herokuapp.com/api/guides/';
 
 //Functions
 
 // Returns All Guides from API
 function fetchGuideData() {
-    const options = {
-        method: 'GET'
-    }
+  const options = {
+    method: "GET",
+  };
 
-    return fetch(GUIDES_BASE_URL, options).then(res => res.json());
-};
+  return fetch(GUIDES_BASE_URL, options).then((res) => res.json());
+}
 
 // Sends New Guides to API
 function addGuideData(guide) {
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-          },
-          body: JSON.stringify(guide)
-        }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(guide),
+  };
 
-        return fetch(GUIDES_BASE_URL, options).then(res => res.json());
-};
+  return fetch(GUIDES_BASE_URL, options).then((res) => res.json());
+}
 
 // Deletes Guides by ID and Removes them from API
 function deleteGuideData(id) {
-    const options = {
-        method: 'DELETE',
-        headers: {
-            'Authorization': 'Bearer ' + getToken()
-        },
-    }
+  const options = {
+    method: "DELETE",
+    headers: {
+      Authorization: "Bearer " + getToken(),
+    },
+  };
 
-    return fetch(GUIDES_BASE_URL + id, options).then(res => res.json());
-};
+  return fetch(GUIDES_BASE_URL + id, options).then((res) => res.json());
+}
 
 // Returns a Single Guide
 function editGuideData(id) {
-    const options = {
-        method: 'GET',
-        headers: {
-            'Authorization': 'Bearer ' + getToken()
-        },
-    }
+  const options = {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + getToken(),
+    },
+  };
 
-    return fetch(GUIDES_BASE_URL + id, options).then(res => res.json());
+  return fetch(GUIDES_BASE_URL + id, options).then((res) => res.json());
 }
 
 // Sends an Updated Guide to the API by ID
 function updateGuideData(id, updatedGuide) {
-    const options = {
-        method: 'PUT',
-        headers: {
-            'Authorization': 'Bearer ' + getToken(),
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(updatedGuide)
-    }
+  const options = {
+    method: "PUT",
+    headers: {
+      Authorization: "Bearer " + getToken(),
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedGuide),
+  };
 
-    return fetch(GUIDES_BASE_URL + id, options).then(res => res.json());
-};
+  return fetch(GUIDES_BASE_URL + id, options).then((res) => res.json());
+}
 
 // Export Functions
 
 export {
-    fetchGuideData,
-    addGuideData,
-    deleteGuideData,
-    updateGuideData,
-    editGuideData
-}
+  fetchGuideData,
+  addGuideData,
+  deleteGuideData,
+  updateGuideData,
+  editGuideData,
+};
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,54 +1,51 @@
 // Imports
-import { setToken, getUserFromToken, removeToken } from './tokenService';
+import { setToken, getUserFromToken, removeToken } from "./tokenService";
 
 // Variables
-// const DATABASE_BASE_URL = 'http://localhost:3001/api/users/';
-const DATABASE_BASE_URL = 'https://league-elite-backend.herokuapp.com/api/users/';
-
+const DATABASE_BASE_URL = "http://localhost:3001/api/users/";
+//const DATABASE_BASE_URL = 'https://league-elite-backend.herokuapp.com/api/users/';
 
 // Functions
 function signup(user) {
-    return fetch(DATABASE_BASE_URL + 'signup', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'Application/json'
-        },
-        body: JSON.stringify(user)
-    }).then(response => {
-        if(response.ok) return response.json();
-        // error handling
-        throw new Error('Email already taken');
-    }).then(data => setToken(data.token))
+  return fetch(DATABASE_BASE_URL + "signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "Application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      if (response.ok) return response.json();
+      // error handling
+      throw new Error("Email already taken");
+    })
+    .then((data) => setToken(data.token));
 }
 
 function login(credentials) {
-    return fetch(DATABASE_BASE_URL + 'login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'Application/json'
-        },
-        body: JSON.stringify(credentials)
-    }).then(response => {
-        if(response.ok) return response.json();
-        // error handling
-        throw new Error('Bad Credentials');
-    }).then(data => setToken(data.token))
+  return fetch(DATABASE_BASE_URL + "login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "Application/json",
+    },
+    body: JSON.stringify(credentials),
+  })
+    .then((response) => {
+      if (response.ok) return response.json();
+      // error handling
+      throw new Error("Bad Credentials");
+    })
+    .then((data) => setToken(data.token));
 }
 
 function logout() {
-    removeToken();
+  removeToken();
 }
 
 function getUser() {
-    return getUserFromToken();
+  return getUserFromToken();
 }
-
 
 // Export Functions
 
-export {
-    signup,
-    login,
-    logout,
-    getUser
-};
+export { signup, login, logout, getUser };
